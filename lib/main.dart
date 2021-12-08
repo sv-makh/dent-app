1768ab7cc313a2f1db518aa082b1bba7498bf529
Milena Bakulina <mb@Milenas-MBP.Dlink>
2021-12-08 17:37:57 +0200

    смена шрифта

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,14 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(backgroundColor: Colors.white70,
         leading: Image.asset("assets/images/icons8-tooth-50.png"),
         title: const Text("Стоматология",style: TextStyle(
-            fontFamily: 'RobotoSlab-ExtraBold', color: Colors.black87
+            fontFamily: 'RocknRollOne-Regular', color: Colors.black87
         )), centerTitle: true),
         // поменялся цвет фона, шрифт, добавлена картинка
         body: MyForm(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.orangeAccent)),
+        floatingActionButton: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.amber)),
           child: const Text("Рассчитать",style: TextStyle(
-              fontFamily: 'RobotoSlab-Bold', color: Colors.black87
+              fontFamily: 'RocknRollOne-Regular', color: Colors.black87
           )),
           // поменялся цвет фона, шрифт
           onPressed: () {
@@ -85,7 +85,8 @@ class MyFormState extends State {
                 child: Column(children: [
               const Text("Тип протезирования",
               style: TextStyle(
-                fontFamily: 'RobotoSlab-Bold'
+                fontSize: 13,
+                fontFamily: 'RocknRollOne-Regular'
                 // поменялся шрифт
               )),
               DropdownButton<String>(
@@ -93,11 +94,14 @@ class MyFormState extends State {
                 icon: const Icon(Icons.arrow_downward),
                 iconSize: 24,
                 elevation: 16,
-                style: const TextStyle(fontFamily: 'RobotoSlab-Bold', color: Colors.lightBlueAccent),
+                style: const TextStyle(
+                fontSize: 11,
+                fontFamily: 'RocknRollOne-Regular',
+                color: Colors.cyan),
                 // поменялся цвет фона, шрифт
                 underline: Container(
                   height: 2,
-                  color: Colors.lightBlueAccent,
+                  color: Colors.cyan,
                 ),
                 onChanged: (String? newValue) {
                   //setState(() {
@@ -113,14 +117,16 @@ class MyFormState extends State {
                 }).toList(),
               ),
               const Text("Коэффициент стабильности имплантанта (ISQ)",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontFamily: 'RobotoSlab-Bold'
+                      fontSize: 13,
+                      fontFamily: 'RocknRollOne-Regular'
                     // поменялся шрифт
                   )),
               SliderTheme(data: SliderThemeData(
-                activeTrackColor: Colors.orange,
-                inactiveTrackColor: Colors.orangeAccent,
-                thumbColor: Colors.orange,
+                activeTrackColor: Colors.amber,
+                inactiveTrackColor: Colors.amberAccent,
+                thumbColor: Colors.amber,
                 // добавилась SliderTheme, чтобы поменялся цвет
               ),
                 child: Slider(
@@ -137,13 +143,15 @@ class MyFormState extends State {
               ),
               ),
               const Text("Динамометрическое усилие, н/см2",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontFamily: 'RobotoSlab-Bold'
+                      fontSize: 13,
+                      fontFamily: 'RocknRollOne-Regular'
                   )),
                 SliderTheme(data: SliderThemeData(
-                  activeTrackColor: Colors.orange,
-                  inactiveTrackColor: Colors.orangeAccent,
-                  thumbColor: Colors.orange,
+                  activeTrackColor: Colors.amber,
+                  inactiveTrackColor: Colors.amberAccent,
+                  thumbColor: Colors.amber,
                 ),
                     child: Slider(
                 value: force.toDouble(),
@@ -160,17 +168,18 @@ class MyFormState extends State {
                 ),
               const Text("Тип фиксации",
                   style: TextStyle(
-                      fontFamily: 'RobotoSlab-Bold'
+                      fontSize: 13,
+                      fontFamily: 'RocknRollOne-Regular'
                   )),
               DropdownButton<String>(
                 value: typeFix,
                 icon: const Icon(Icons.arrow_downward),
                 iconSize: 24,
                 elevation: 16,
-                style: const TextStyle(fontFamily: 'RobotoSlab-Bold',color: Colors.lightBlueAccent),
+                style: const TextStyle(fontSize: 11,fontFamily: 'RocknRollOne-Regular',color: Colors.cyan),
                 underline: Container(
                   height: 2,
-                  color: Colors.lightBlueAccent,
+                  color: Colors.cyan,
                 ),
                 onChanged: (String? newValue) {
                   //setState(() {
